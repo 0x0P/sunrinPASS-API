b1762f9108529bd8a3e02b7c46d7060c9e18a8c8
PERCENTZERO <percentzero@outlook.com>
2024-12-09 10:11:20 +0900

    user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString, IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsBoolean()
+  isTeacher: boolean;
+}
